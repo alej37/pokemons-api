1814e111d140221c61b71a32b65baa60e70db0d9
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2021-07-11 23:01:49 -0400

    added destroy test

--- spec/api/controller/pokemons_controller_spec.rb
+++ spec/api/controller/pokemons_controller_spec.rb
@@ -1,16 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::PokemonsController, type: :controller do
-  describe '.show' do
+   describe '.show' do
     subject { get :show, params: params }
 
-    context 'given an invalid id' do
+    context 'with an non existing id' do
       let(:params) do
-        { id: 802 }
+        { id: 805 }
       end
 
-      it 'should return a non found error' do
+      it 'should returns an not found error with a non existing id' do
         expect(subject.status).to eq(404)
+      end
+    end
+  end
+
+  describe '.destroy' do
+    context 'test destroy routing' do
+      let!(:pokemon) { Pokemon.create(name: 'pikachu') }
+
+      it 'should delete a created pokemon' do
+        expect(Pokemon.find_by(name: 'pikachu')).to_not be_nil
+        post :destroy, params: { id: pokemon.id }
+        expect(Pokemon.find_by(name: 'pikachu')).to be_nil
       end
     end
   end
